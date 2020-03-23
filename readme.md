8cafc841fd32b31e25c413fc9c20a7ba3ad10a11
Tangguh Pramono <tangguhmyname@gmail.com>
2020-03-23 14:10:31 +0700

    Inisialisasi project

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Covid-19-PWA
+Edukasi web tentang virus Covid-19 yang berisikan info mengenai Covid-19, cara pencegahan, ciri ciri penderita dan data penderita di indonesia
+Menggunakan data dari [mathroid](https://github.com/mathdroid/indonesia-covid-19-api)
+
+## Fitur
+1. Pengenalan Covid-19
+3. Menampilakan lokasi
+4. Edukasi Covid-19 seperti ciri-ciri dan pencegahan
+5. Berita tentang Covid-19
+6. Timeline Covid-19
+
+## Catatan
+Jika ingin menambahkan file seperti script dan media pastikan mencantumkan ke file serviceworker nya di sw.js
---
+++ sw.js
@@ -0,0 +1,28 @@
+var cacheName = 'web-app-v1';
+var staticAssets = [
+    '/',
+    '/index.html',
+    '/assets/style/hello-world.css',
+    '/index.js',
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(staticAssets);
+        })
+    );
+});
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request, { ignoreSearch: true }).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
