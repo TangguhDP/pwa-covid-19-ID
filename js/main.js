230577862aa53f9216b47ab951a046831fd53b6f
Ichwanul Fadhli <ichwanulf60@gmail.com>
2020-04-08 01:11:58 +0700

    Koreksi minor pada jquery

--- js/main.js
+++ js/main.js
@@ -266,12 +266,11 @@ jQuery(document).ready(function($) {
 				$('.block-counter-1-number').each(function(){
 					var $this = $(this),
 						num = $this.data('number');
-						console.log(num);
 					$this.animateNumber(
 					  {
 					    number: num,
 					    numberStep: comma_separator_number_step
-					  }, 7000
+					  }, 3000
 					);
 				});
 				
