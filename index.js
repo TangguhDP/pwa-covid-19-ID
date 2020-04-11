3e5af1188bf98add0d3e9ea01cb95f6cd0ee6b18
dhimasPH <dhimasherlambang@gmail.com>
2020-04-11 13:11:45 +0700

    fixing sw.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ async function registerSW() {
   if ('serviceWorker' in navigator) {
     try {
       await navigator.serviceWorker.register('./sw.js');
+      console.log(`SW registration successfully`);
     } catch (e) {
       console.log(`SW registration failed`);
     }
